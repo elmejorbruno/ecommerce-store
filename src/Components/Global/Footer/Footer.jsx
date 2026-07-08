0921367c94067274c6e453e4cf96890343a67271
elmejorbruno <brunoponceprop@gmail.com>
2026-07-08 16:53:54 -0400

    Se agrego el archivo que contendra el pie de pagina llamado Footer

---
+++ src/Components/Global/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <section className="bg-slate-950 text-gray-300">
+    <div className="container py-16">
+
+    </div>
+</section>
+  )
+}
+
+export default Footer

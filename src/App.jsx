d480aa15a523e358e3593c94429bfa2bb7f1a8ee
elmejorbruno <brunoponceprop@gmail.com>
2026-07-08 15:36:46 -0400

    ya no se usa el archivo App como layout y se creo un archivo para esto llamado Layout, ahora App se usa como route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import Products from './Components/Products/Products';
 import AOS from 'aos';
 import "aos/dist/aos.css";
 import TopProducts from './Components/TopProducts/TopProducts';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Women from './Pages/Women';
+import Men from './Pages/Men';
+import Kids from './Pages/Kids';
+import NotFound from './Pages/NotFound';
+import Layout from './Layout/Layout';
 
 const App = () => {
 	React.useEffect(() => {
@@ -17,12 +24,20 @@ const App = () => {
     AOS.refresh();
 	}, []);
 	return (
-		<div>
-			<Navbar />
-			<Hero />
-			<Products />
-			<TopProducts/>
-		</div>
+		<>
+		
+		<Routes>
+			<Route element={<Layout/>} >
+			<Route path='/' element={<Home/>} />
+				<Route path='women' element={<Women/>} />
+				<Route path='men' element={<Men/>} />
+				<Route path='kids' element={<Kids/>} />
+				<Route path='notfound' element={<NotFound/>} />
+			</Route>
+			
+		</Routes>
+			
+		</>
 	);
 };
 

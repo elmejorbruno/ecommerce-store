8ef1bf634bfa41a027768760cc02ad1abf6aee7b
elmejorbruno <brunoponceprop@gmail.com>
2026-07-09 17:08:49 -0400

    Se cambio de carpeta y quedo dentro de HomeSection

--- src/Components/HomeSection/Banner/Banner.jsx
+++ src/Components/HomeSection/Banner/Banner.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import ImageChica from '../../assets/img/women/modelo-blusa-negra.png'
-import { GrSecure } from 'react-icons/gr'
+import ImageChica from '../../../assets/img/women/modelo-blusa-negra.png'
 import { IoFastFood } from 'react-icons/io5'
+import { GrSecure } from 'react-icons/gr'
 
 export const Banner = () => {
   return (
     <div className='min-h-[550px] flex 
-    justify-center items-center py-12 sm:py-0 '>
-        <div className="container">
+    justify-center items-center py-12 sm:py-0 m-10'>
+        <div className="container ">
             <div className="grid 
             grid-cols-1 sm:grid-cols-2 
-            gap-6 items-center" >
+            gap-6 items-center bg-hero/30 p-4" >
                 {/* imagen */}
                 <div data-aos="zoom-in" className="">
-                    <img src={ImageChica} alt="Imagen del banner" className="max-w-[250px] h-[370px] w-full
+                    <img src={ImageChica} alt="Imagen del banner" className="max-w-[450px] h-[auto] w-full
                     max-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,0.1) object-cover]" />
                 </div>
                 {/* text */}

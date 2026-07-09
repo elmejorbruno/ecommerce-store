2c83d76a750d3fc07c0962439707fbfc96827085
elmejorbruno <brunoponceprop@gmail.com>
2026-07-09 15:37:23 -0400

    Se agrego la el efecto para que al navegar por las paginas siempre muestre el inicio de la pagina y se agrego la rutas para las card o card que contiene informacion y producto

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/Hero/Hero';
-import Products from './Components/Products/Products';
+import Navbar from './Components/Global/Navbar/Navbar';
+import Hero from './Components/HomeSection/Hero/Hero';
+import Products from './Components/HomeSection/FeaturedProducts';
 import AOS from 'aos';
-import "aos/dist/aos.css";
+import 'aos/dist/aos.css';
 import TopProducts from './Components/TopProducts/TopProducts';
 import { Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home';
-import Women from './Pages/Women';
-import Men from './Pages/Men';
-import Kids from './Pages/Kids';
+import Home from './Pages/Home/Home';
+import Women from './Pages/Women/Women';
+import Men from './Pages/Men/Men';
+import Kids from './Pages/Kids/Kids';
 import NotFound from './Pages/NotFound';
 import Layout from './Layout/Layout';
+import ProductDetails from './Pages/ProductDetails';
+import ScrollToTop from './Components/Global/ScrollToTop';
 
 const App = () => {
 	React.useEffect(() => {
@@ -21,22 +23,23 @@ const App = () => {
 			easing: 'ease-in-sine',
 			delay: 100,
 		});
-    AOS.refresh();
+		AOS.refresh();
 	}, []);
 	return (
 		<>
-		
-		<Routes>
-			<Route element={<Layout/>} >
-			<Route path='/' element={<Home/>} />
-				<Route path='women' element={<Women/>} />
-				<Route path='men' element={<Men/>} />
-				<Route path='kids' element={<Kids/>} />
-				<Route path='notfound' element={<NotFound/>} />
-			</Route>
-			
-		</Routes>
-			
+		<ScrollToTop/>
+			<Routes>
+				<Route element={<Layout />}>
+					<Route path="/" element={<Home />} />
+					<Route path="women" element={<Women />} />
+					<Route path="men" element={<Men />} />
+					<Route path="kids" element={<Kids />} />
+					<Route
+					path='/product/:id'
+					element={<ProductDetails/>} />
+					<Route path="notfound" element={<NotFound />} />
+				</Route>
+			</Routes>
 		</>
 	);
 };

87724f77936c98b546a145cd903e8d7a4ef02da3
elmejorbruno <brunoponceprop@gmail.com>
2026-07-09 17:48:43 -0400

    Se cambio el archivo Hero categoria para un archivo de HomeSection

---
+++ src/Components/HomeSection/Categoria/Categoria.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Categoria = () => {
+  return (
+    <div>
+        <div className="container">
+            <h1 className='flex'></h1>
+        </div>
+    </div>
+  )
+}
+
+export default Categoria

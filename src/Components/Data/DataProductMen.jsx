f5eb2664129e82599741a2e952d5dfa3d3a5c696
elmejorbruno <brunoponceprop@gmail.com>
2026-07-23 21:12:53 -0400

    Se agrego los archivo para alojar los titulo imagenes y otros datos en una data solo al arhcivo women tiene productos los archivo DataProductKids.jsx DataProductMen.jsx DataProductWomen.jsx

---
+++ src/Components/Data/DataProductMen.jsx
@@ -0,0 +1,63 @@
+ 
+export const dataProductMen = [
+     {
+              id: 1,
+              category: "women",
+              title: "Vestido elegante",
+              img:Img1,
+      
+              rating:5.0,
+              price:49.99,
+              
+              oldPrice:69.99,
+              badge:"Nuevo",
+      
+              sizes:["S","M","L"],
+              
+              colors:[
+               {  name: "Negro",
+                  code: "#000000" 
+      
+               },
+               { 
+                  name:"Gris",
+                  code:"#808080"
+               },
+              ],
+              description: 
+                  "Vestido elegante confeccionado con tela suave y ligera. Ideal para eventos, reuniones.",
+              stock:15,
+              sku:"WE-001",
+              aosDelay: "0"
+          },
+          {
+              id: 2,
+              title: "Blusa Casual y falda",
+              category:"women",
+              img:ModeloBlusaBlaca,
+              rating:4.5,
+               price:49.99,
+      
+               oldPrice:69.99,
+              badge:"Nuevo", 
+      
+              sizes:["S","M"],
+              
+              colors:[
+               {  name: "Negro",
+                  code: "#000000" 
+      
+               },
+               { 
+                  name:"Blanco",
+                  code:"#f1f1f1"
+               },
+              ],
+              description: 
+                  "Blusa elegante, ligera. Ideal para eventos, reuniones o una salida casual.",
+              stock:15,
+              sku:"WE-001",
+              
+              aosDelay: "0"
+          },
+]

e1025a7c6209ee239fde58e0fc7458baf9dc1171
elmejorbruno <brunoponceprop@gmail.com>
2026-07-31 10:21:51 -0400

    Se creo los archivo que contendran la data  o informacion de los coleccion

---
+++ src/Components/Data/collectionsData/WomenCollectionsData.jsx
@@ -0,0 +1,24 @@
+import ModeloTrageCompleto from "../../../assets/img/women/modelo-blusa-blanca-pantalo-blanco.png"
+import VestidoNocheCrema from "../../../assets/img/women/vestido-noche-elegante-crema.png"
+
+export const womenCollections = [
+ {
+    id: 1,
+    title: "Executive Sets",
+    category: "women",
+    slug: "executive-sets",
+    image: ModeloTrageCompleto,
+    description:
+      "Elegant coordinated outfits for a sophisticated professional look.",
+  },
+
+  {
+    id: 2,
+    title: "Evening Dresses",
+    category: "women",
+    slug: "evening-dresses",
+    image: VestidoNocheCrema,
+    description:
+      "Elegant dresses for evening events and special occasions.",
+  },
+];

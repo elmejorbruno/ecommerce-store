db7c9489be6fffcd6ca9fe4d6e56ceaf6959096e
elmejorbruno <brunoponceprop@gmail.com>
2026-08-06 16:55:21 -0400

    Se agrego mas colecciones que tenia en otro archivo cuando empece y se agrego las imagenes optimizadas  en el archivo WomenCollectionsData.jsx

--- src/Components/Data/collectionsData/WomenCollectionsData.jsx
+++ src/Components/Data/collectionsData/WomenCollectionsData.jsx
@@ -1,24 +1,101 @@
-import ModeloTrageCompleto from "../../../assets/img/women/modelo-blusa-blanca-pantalo-blanco.png"
-import VestidoNocheCrema from "../../../assets/img/women/vestido-noche-elegante-crema.png"
+import ModeloTrageCompleto from "../../../assets/img/women/modelo-blusa-blanca-pantalo-blanco.webp"
+import VestidoNocheCrema from "../../../assets/img/women/vestido-noche-elegante-crema.webp"
+import VestidoEleganteGris from "../../../assets/img/women/vestido-elegante-gris.webp"
+import TrajeElegante from "../../../assets/img/women/modelo-blusa-blanca.webp"
+import ModeloDosPiezaCamisaMinifalda from "../../../assets/img/women/img-4.4.webp"
 
 export const womenCollections = [
  {
     id: 1,
-    title: "Executive Sets",
+    title: "Traje de oficina completo",
     category: "women",
     slug: "executive-sets",
     image: ModeloTrageCompleto,
+    
+        rating:5.0,
+        price:49.99,
+        
+        oldPrice:69.99,
+        badge:"Nuevo",
+
+        sizes:["S","M","L"],
+        
     description:
       "Elegant coordinated outfits for a sophisticated professional look.",
   },
 
   {
     id: 2,
-    title: "Evening Dresses",
+    title: "Vestido de noche satinado",
     category: "women",
     slug: "evening-dresses",
     image: VestidoNocheCrema,
+    
+        rating:5.0,
+        price:49.99,
+        
+        oldPrice:69.99,
+        badge:"Nuevo",
+
+        sizes:["S","M","L"],
+        
     description:
       "Elegant dresses for evening events and special occasions.",
   },
+   {
+    id: 3,
+    title: "Vestido de fiesta elegante",
+    category: "women",
+    slug: "evening-party-dresses",
+    image: VestidoEleganteGris,
+    
+        rating:5.0,
+        price:49.99,
+        
+        oldPrice:69.99,
+        badge:"Nuevo",
+
+        sizes:["S","M","L"],
+        
+    description:
+      "Elegant dresses for evening events and special occasions.",
+  },
+   {
+    id: 4,
+    title: "camisa y falda ejecutivo",
+    category: "women",
+    slug: "traje-ejecutivo",
+    image: TrajeElegante,
+    
+        rating:5.0,
+        price:49.99,
+        
+        oldPrice:69.99,
+        badge:"Nuevo",
+
+        sizes:["S","M","L"],
+        
+    description:
+      "Elegant dresses for evening events and special occasions.",
+  },
+ 
+   {
+    id: 5,
+    title: "Camisa, chaqueta y minifalda informal",
+    category: "women",
+    slug: "falda-chaqueta-camisa",
+    image: ModeloDosPiezaCamisaMinifalda,
+    
+        rating:5.0,
+        price:49.99,
+        
+        oldPrice:69.99,
+        badge:"Nuevo",
+
+        sizes:["S","M","L"],
+        
+    description:
+      "Elegant dresses for evening events and special occasions.",
+  },
+
 ];

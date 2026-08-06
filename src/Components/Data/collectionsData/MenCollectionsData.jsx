9041d02cff85709c389c022ac24c32b99bbb17dd
elmejorbruno <brunoponceprop@gmail.com>
2026-08-06 16:53:57 -0400

    Se agrego la estrutura previa para comenzar en el archivos MenCollectionsData.jsx

---
+++ src/Components/Data/collectionsData/MenCollectionsData.jsx
@@ -0,0 +1,26 @@
+import ModeloCamisaNegra from "../../../assets/img/men/modelo-men-camisa-negra.png"
+import ModeloDosPiezaCamisaMinifalda from "../../../assets/img/img-4.4.png"
+
+export const womenCollections = [
+
+   {
+    id: 1,
+    title: "Camisa manga larga",
+    category: "men",
+    slug: "evening-dresses",
+    image: ModeloCamisaNegra,
+    
+        rating:5.0,
+        price:39.99,
+        
+        oldPrice:69.99,
+        badge:"Nuevo",
+
+        sizes:["S","M","L"],
+        
+    description:
+      "Elegant dresses for evening events and special occasions.",
+  },
+
+
+];
